f896a6779c9e9780288998774dc6539d311f0fa9
Grzegorz Kozłowski <grzegorz.kozlowski2@gmail.com>
2023-09-18 15:07:37 +0200

    According to suggestions, I improved the program: I moved the HelperLibrary helper class to the HelperLibrary.cs file For the methods, I corrected camelCase to PascalCase. I made some code simplifications. I used string.Empty instead of ""

---
+++ ReverseOrderApp/HelperLibrary.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ReverseOrderApp
+{
+        internal class HelperLibrary
+        {            
+            public string GetCharacter(string valueToCheck)
+            {
+                string properValue = valueToCheck;
+                while (properValue.Length != 1)
+                {
+                    Console.WriteLine("You entered more than 1 character. Try again:");
+                    properValue = Console.ReadLine();
+                }
+                return properValue;
+            }
+
+            public string CreateStringToReverse (string stringToReverse, int characterIndex)
+            {                                
+                Console.WriteLine($"Character {characterIndex}: ");
+                string enteredValue = Console.ReadLine();
+                stringToReverse = stringToReverse + enteredValue;
+                return stringToReverse;
+            }
+
+            public int NumberOfCharacters()
+            {
+                string inputValue = Console.ReadLine();
+                bool correctValue = false;
+                string errorInfo = ""; // = "" - Protection against using an unassigned variable in Console.WriteLine.                                
+                int numberOfCharacters = 0; // Protection against "Compiler Error CS0177" 
+                while (!correctValue)
+                {
+                    if (!Regex.IsMatch(inputValue, @"^[0-9]+$"))
+                    {
+                        errorInfo = "Entered value is not a number. Try once again:";
+                    } 
+                    else
+                    {
+                        numberOfCharacters = int.Parse(inputValue);
+                        if (!(numberOfCharacters >= 2 && numberOfCharacters <= 10))
+                        {
+                            errorInfo = "Entered value is not in 2-10 range. Try once again:";
+
+                        }
+                        else
+                        {
+                            correctValue = true;
+                        }
+                    }
+                    if (!correctValue)
+                    {
+                        Console.WriteLine($"{errorInfo}");
+                        inputValue = Console.ReadLine();
+                    }
+
+                }
+                return numberOfCharacters;
+            }
+
+            public string GetReversedText (string inputString)
+            {
+                char[] stringArray = inputString.ToCharArray();
+                Array.Reverse(stringArray);
+                string reversedStr = new string (stringArray);
+                return reversedStr;
+            }
+        }
+        
+
+    
+}
